bef95c2fbb289f5d96bec7b43ca1bf4a245a06a8
YK_ <Bo.bolweg@gmail.com>
2024-09-30 09:19:10 +0200

    meow

--- 02 rekenen/01 HowManyClicks/HowManyClicks/Form1.cs
+++ 02 rekenen/01 HowManyClicks/HowManyClicks/Form1.cs
@@ -2,19 +2,19 @@ namespace HowManyClicks
 {
     public partial class Form1 : Form
     {
-        int clicks = 1
+        int clicks = 1;
         //1) maak hier een variable (int met als naam clicks)
-        private ???
+        private int clicks;
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(clicks);
         }
 
         private void plusButton_Click(object sender, EventArgs e)
         {
 
             //2) tel hier 1 op bij clicks (zie boven in de klas)
-            ???
+            clicks ++;
             clicksAmountText.Text = clicks.ToString();
         }
     }
